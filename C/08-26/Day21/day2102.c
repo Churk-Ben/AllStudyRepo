95f1a53c806da48d8f3c44dc459a9cc9d40d8592
churk_ben <2779675416@qq.com>
2026-03-10 23:21:00 +0800

    update

--- C/08-26/Day21/day2102.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int a[3]={1,2,3};
-    printf("%p\n",a[0]);
-    printf("%d\n",a[4]);//它是完全不查啊
-    return 0;   
-}
--- Study_of_Python/Day03/day0302.py
+++
@@ -1,8 +0,0 @@
-#
-s=input().split()
-print(int(s[0])+int(s[1]))
-#
-print(sum(map(int, input().split())))
-#
-s = list(map(int, input().split()))
-print(sum(s))
