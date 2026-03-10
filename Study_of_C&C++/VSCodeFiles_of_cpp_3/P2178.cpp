6632001344fdc168da3d40c5fcf32be3665e0b7a
churk_ben <2779675416@qq.com>
2026-03-10 13:02:33 +0800

    Refactor code structure for improved readability and maintainability

---
+++ Study_of_C&C++/VSCodeFiles_of_cpp_3/P2178.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Task
+{
+    string name;
+    int needed;
+    int spent = 0;
+};
+
+int main()
+{
+    int N, Q, t = 0;
+    queue<Task> q;
+    vector<Task> p;
+
+    cin >> N >> Q;
+
+    for (int i = 0; i < N; i++)
+    {
+        Task p;
+        cin >> p.name >> p.needed;
+        q.push(p);
+    }
+
+    while (!q.empty())
+    {
+        Task cur = q.front();
+        if (cur.needed > Q)
+        {
+            cur.needed -= Q;
+            q.push(cur);
+            q.pop();
+            t += Q;
+        }
+        else
+        {
+            cur.spent = t + cur.needed;
+            t += cur.needed;
+            p.push_back(cur);
+            q.pop();
+        }
+    }
+
+    for (auto task : p)
+    {
+        cout << task.name << " " << task.spent << endl;
+    }
+
+    return 0;
+}
