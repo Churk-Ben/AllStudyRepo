d9030b55fd1e04c7d3079d6ff8f1101950d5d5f2
Churk <2779675416@qq.com>
2026-04-09 08:20:50 +0800

    update

---
+++ MATLAB/Day01/day0101.m
@@ -0,0 +1,8 @@
+Fig0222_b__cameraman_ = imread("E:\Coding Projects\Programme-Learning-Repo\MATLAB\Day01\Fig0222(b)(cameraman).tif");
+imshow(Fig0222_b__cameraman_)
+
+imfinfo("Fig0222(b)(cameraman).tif")
+
+imwrite(Fig0222_b__cameraman_,"Fig0222(b)(cameraman).bmp","bmp")
+Fig0222_b__cameraman__bmp = imread("Fig0222(b)(cameraman).bmp");
+imshow(Fig0222_b__cameraman__bmp)
